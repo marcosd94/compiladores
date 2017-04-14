7ca0d55ca5b593eeb4b955fc7857b463dcd4cf49
Marcos Peralta <mrcperalta.mp@gmail.com>
2017-04-14 12:44:15 -0400

    Se agrega la verifiación de los tokens y se modifica el readme

--- C/anlex.c
+++ C/anlex.c
@@ -41,6 +41,7 @@ void error(const char* mensaje)
 	printf("Lin %d: Error Lexico. %s.\n",numLinea,mensaje);	
 }
 
+
 void sigLex()
 {
 	int i=0;
@@ -52,7 +53,6 @@ void sigLex()
 
 	while((c=fgetc(archivo))!=EOF)
 	{
-		
 		if (c==' ' || c=='\t')
 			continue;	//eliminar espacios en blanco
 		else if(c=='\n')
@@ -60,394 +60,196 @@ void sigLex()
 			//incrementar el numero de linea
 			numLinea++;
 			continue;
-		}
-		else if (isalpha(c))
-		{
-			//es un identificador (o palabra reservada)
-			i=0;
-			do{
-				id[i]=c;
-				i++;
-				c=fgetc(archivo);
-				if (i>=TAMLEX)
-					error("Longitud de Identificador excede tamaño de buffer");
-			}while(isalpha(c) || isdigit(c));
-			id[i]='\0';
-			if (c!=EOF)
-				ungetc(c,archivo);
-			else
-				c=0;
-			t.pe=buscar(id);
-			t.compLex=t.pe->compLex;
-			if (t.pe->compLex==-1)
-			{
-				strcpy(e.lexema,id);
-				e.compLex=ID;
-				insertar(e);
-				t.pe=buscar(id);
-				t.compLex=ID;
-			}
-			break;
-		}
-		else if (isdigit(c))
-		{
-				//es un numero
-				i=0;
-				estado=0;
-				acepto=0;
-				id[i]=c;
-				
-				while(!acepto)
-				{
-					switch(estado){
-					case 0: //una secuencia netamente de digitos, puede ocurrir . o e
-						c=fgetc(archivo);
-						if (isdigit(c))
-						{
-							id[++i]=c;
-							estado=0;
-						}
-						else if(c=='.'){
-							id[++i]=c;
-							estado=1;
-						}
-						else if(tolower(c)=='e'){
-							id[++i]=c;
-							estado=3;
-						}
-						else{
-							estado=6;
-						}
-						break;
-					
-					case 1://un punto, debe seguir un digito (caso especial de array, puede venir otro punto)
-						c=fgetc(archivo);						
-						if (isdigit(c))
-						{
-							id[++i]=c;
-							estado=2;
-						}
-						else if(c=='.')
-						{
-							i--;
-							fseek(archivo,-1,SEEK_CUR);
-							estado=6;
-						}
-						else{
-							sprintf(msg,"No se esperaba '%c'",c);
-							estado=-1;
-						}
-						break;
-					case 2://la fraccion decimal, pueden seguir los digitos o e
-						c=fgetc(archivo);
-						if (isdigit(c))
-						{
-							id[++i]=c;
-							estado=2;
-						}
-						else if(tolower(c)=='e')
-						{
-							id[++i]=c;
-							estado=3;
-						}
-						else
-							estado=6;
-						break;
-					case 3://una e, puede seguir +, - o una secuencia de digitos
-						c=fgetc(archivo);
-						if (c=='+' || c=='-')
-						{
-							id[++i]=c;
-							estado=4;
-						}
-						else if(isdigit(c))
-						{
-							id[++i]=c;
-							estado=5;
-						}
-						else{
-							sprintf(msg,"No se esperaba '%c'",c);
-							estado=-1;
-						}
-						break;
-					case 4://necesariamente debe venir por lo menos un digito
-						c=fgetc(archivo);
-						if (isdigit(c))
-						{
-							id[++i]=c;
-							estado=5;
-						}
-						else{
-							sprintf(msg,"No se esperaba '%c'",c);
-							estado=-1;
-						}
-						break;
-					case 5://una secuencia de digitos correspondiente al exponente
-						c=fgetc(archivo);
-						if (isdigit(c))
-						{
-							id[++i]=c;
-							estado=5;
-						}
-						else{
-							estado=6;
-						}break;
-					case 6://estado de aceptacion, devolver el caracter correspondiente a otro componente lexico
-						if (c!=EOF)
-							ungetc(c,archivo);
-						else
-							c=0;
-						id[++i]='\0';
-						acepto=1;
-						t.pe=buscar(id);
-						if (t.pe->compLex==-1)
-						{
-							strcpy(e.lexema,id);
-							e.compLex=NUM;
-							insertar(e);
-							t.pe=buscar(id);
-						}
-						t.compLex=NUM;
-						break;
-					case -1:
-						if (c==EOF)
-							error("No se esperaba el fin de archivo");
-						else
-							error(msg);
-						exit(1);
-					}
-				}
-			break;
-		}
-		else if (c=='<') 
-		{
-			//es un operador relacional, averiguar cual
-			c=fgetc(archivo);
-			if (c=='>'){
-				t.compLex=OPREL;
-				t.pe=buscar("<>");
-			}
-			else if (c=='='){
-				t.compLex=OPREL;
-				t.pe=buscar("<=");
-			}
-			else{
-				ungetc(c,archivo);
-				t.compLex=OPREL;
-				t.pe=buscar("<");
-			}
-			break;
-		}
-		else if (c=='>')
-		{
-			//es un operador relacional, averiguar cual
-				c=fgetc(archivo);
-			if (c=='='){
-				t.compLex=OPREL;
-				t.pe=buscar(">=");
-			}
-			else{
-				ungetc(c,archivo);
-				t.compLex=OPREL;
-				t.pe=buscar(">");
-			}
-			break;
-		}
-		else if (c==':')
-		{
-			//puede ser un : o un operador de asignacion
-			c=fgetc(archivo);
-			if (c=='='){
-				t.compLex=OPASIGNA;
-				t.pe=buscar(":=");
-			}
-			else{
-				ungetc(c,archivo);
-				t.compLex=':';
-				t.pe=buscar(":");
-			}
-			break;
-		}
-		else if (c=='+')
-		{
-			t.compLex=OPSUMA;
-			t.pe=buscar("+");
-			break;
-		}
-		else if (c=='-')
-		{
-			t.compLex=OPSUMA;
-			t.pe=buscar("-");
-			break;
-		}
-		else if (c=='*')
-		{
-			t.compLex=OPMULT;
-			t.pe=buscar("*");
-			break;
-		}
-		else if (c=='/')
-		{
-			t.compLex=OPMULT;
-			t.pe=buscar("/");
-			break;
-		}
-		else if (c=='=')
-		{
-			t.compLex=OPREL;
-			t.pe=buscar("=");
-			break;
-		}
-		else if (c==',')
-		{
-			t.compLex=',';
-			t.pe=buscar(",");
-			break;
-		}
-		else if (c==';')
-		{
-			t.compLex=';';
-			t.pe=buscar(";");
-			break;
-		}
-		else if (c=='.')
-		{
-			t.compLex='.';
-			t.pe=buscar(".");
-			break;
-		}
-		else if (c=='(')
-		{
-			if ((c=fgetc(archivo))=='*')
-			{//es un comentario
-				while(c!=EOF)
-				{
-					c=fgetc(archivo);
-					if (c=='*')
-					{
-						if ((c=fgetc(archivo))==')')
-						{
-							break;
-						}
-					}
-					else if(c=='\n')
-					{
-						//incrementar el numero de linea
-						numLinea++;
-					}
-				}
-				if (c==EOF)
-					error("Se llego al fin de archivo sin finalizar un comentario");
-				continue;
-			}
-			else
-			{
-				ungetc(c,archivo);
-				t.compLex='(';
-				t.pe=buscar("(");
-			}
-			break;
-		}
-		else if (c==')')
-		{
-			t.compLex=')';
-			t.pe=buscar(")");
-			break;
-		}
+		}else if (c==':')
+        {
+            t.compLex=DOS_PUNTOS;
+            t.pe=buscar(":");
+            break;
+        }
 		else if (c=='[')
 		{
-			t.compLex='[';
-			t.pe=buscar("[");
-			break;
+		    t.compLex=L_CORCHETE;
+		    t.pe=buscar("[");
+		    break;
 		}
 		else if (c==']')
 		{
-			t.compLex=']';
-			t.pe=buscar("]");
-			break;
+		    t.compLex=R_CORCHETE;
+		    t.pe=buscar("]");
+		    break;
 		}
-		else if (c=='\'')
-		{//un caracter o una cadena de caracteres
-			i=0;
-			id[i]=c;
-			i++;
-			do{
-				c=fgetc(archivo);
-				if (c=='\'')
-				{
-					c=fgetc(archivo);
-					if (c=='\'')
-					{
-						id[i]=c;
-						i++;
-						id[i]=c;
-						i++;
-					}
-					else
-					{
-						id[i]='\'';
-						i++;
-						break;
-					}
-				}
-				else if(c==EOF)
-				{
-					error("Se llego al fin de archivo sin finalizar un literal");
-				}
-				else{
+		else if (c=='(')
+		{
+		    t.compLex=L_LLAVE;
+		    t.pe=buscar("(");
+		    break;
+		}
+		else if (c==')')
+		{
+		    t.compLex=R_LLAVE;
+		    t.pe=buscar(")");
+		    break;
+		}
+		else if (c==',')
+		{
+		    t.compLex=COMA;
+		    t.pe=buscar(",");
+		    break;
+		}
+			else if (c == '"')
+			{
+			//es un STRING /*VERIFICA QUE SE INGRESA UN STRING VALIDO*/
+		    c=fgetc(archivo);
+		    while(c!=EOF){
+		        if(c == '"'){
+		            t.compLex=STRING;
+		            t.pe=buscar("STRING"); //??????????
+		            break;
+		        }else{
+		           c=fgetc(archivo);
+		        }
+		    }
+		    if (c==EOF)
+		        error("Se llego al fin sin cerrar el String");
+			}
+			else if (isdigit(c))
+			{
+					//es un numero
+					i=0;
+					estado=0;
+					acepto=0;
 					id[i]=c;
-					i++;
-				}
-			}while(isascii(c));
-			id[i]='\0';
-			if (c!=EOF)
-				ungetc(c,archivo);
-			else
-				c=0;
-			t.pe=buscar(id);
-			t.compLex=t.pe->compLex;
-			if (t.pe->compLex==-1)
-			{
-				strcpy(e.lexema,id);
-				if (strlen(id)==3 || strcmp(id,"''''")==0)
-					e.compLex=CAR;
-				else
-					e.compLex=LITERAL;
-				insertar(e);
-				t.pe=buscar(id);
-				t.compLex=e.compLex;
-			}
-			break;
+				
+					while(!acepto)
+					{
+						switch(estado){
+						case 0: //una secuencia netamente de digitos, puede ocurrir . o e
+							c=fgetc(archivo);
+							if (isdigit(c))
+							{
+								id[++i]=c;
+								estado=0;
+							}
+							else if(c=='.'){
+								id[++i]=c;
+								estado=1;
+							}
+							else if(tolower(c)=='e'){
+								id[++i]=c;
+								estado=3;
+							}
+							else{
+								estado=6;
+							}
+							break;
+					
+						case 1://un punto, debe seguir un digito (caso especial de array, puede venir otro punto)
+							c=fgetc(archivo);						
+							if (isdigit(c))
+							{
+								id[++i]=c;
+								estado=2;
+							}
+							else if(c=='.')
+							{
+								i--;
+								fseek(archivo,-1,SEEK_CUR);
+								estado=6;
+							}
+							else{
+								sprintf(msg,"No se esperaba '%c'",c);
+								estado=-1;
+							}
+							break;
+						case 2://la fraccion decimal, pueden seguir los digitos o e
+							c=fgetc(archivo);
+							if (isdigit(c))
+							{
+								id[++i]=c;
+								estado=2;
+							}
+							else if(tolower(c)=='e')
+							{
+								id[++i]=c;
+								estado=3;
+							}
+							else
+								estado=6;
+							break;
+						case 3://una e, puede seguir +, - o una secuencia de digitos
+							c=fgetc(archivo);
+							if (c=='+' || c=='-')
+							{
+								id[++i]=c;
+								estado=4;
+							}
+							else if(isdigit(c))
+							{
+								id[++i]=c;
+								estado=5;
+							}
+							else{
+								sprintf(msg,"No se esperaba '%c'",c);
+								estado=-1;
+							}
+							break;
+						case 4://necesariamente debe venir por lo menos un digito
+							c=fgetc(archivo);
+							if (isdigit(c))
+							{
+								id[++i]=c;
+								estado=5;
+							}
+							else{
+								sprintf(msg,"No se esperaba '%c'",c);
+								estado=-1;
+							}
+							break;
+						case 5://una secuencia de digitos correspondiente al exponente
+							c=fgetc(archivo);
+							if (isdigit(c))
+							{
+								id[++i]=c;
+								estado=5;
+							}
+							else{
+								estado=6;
+							}break;
+						case 6://estado de aceptacion, devolver el caracter correspondiente a otro componente lexico
+							if (c!=EOF)
+								ungetc(c,archivo);
+							else
+								c=0;
+							id[++i]='\0';
+							acepto=1;
+							t.pe=buscar(id);
+							if (t.pe->compLex==-1)
+							{
+								strcpy(e.lexema,id);
+								e.compLex=NUM;
+								insertar(e);
+								t.pe=buscar(id);
+							}
+							t.compLex=NUM;
+							break;
+						case -1:
+							if (c==EOF)
+								error("No se esperaba el fin de archivo");
+							else
+								error(msg);
+							exit(1);
+						}
+					}
+				break;
 		}
-		else if (c=='{')
+		if (c==EOF)
 		{
-			//elimina el comentario
-			while(c!=EOF)
-			{
-				c=fgetc(archivo);
-				if (c=='}')
-					break;
-				else if(c=='\n')
-				{
-					//incrementar el numero de linea
-					numLinea++;
-				}
-			}
-			if (c==EOF)
-				error("Se llego al fin de archivo sin finalizar un comentario");
+			t.compLex=EOF;
+			// strcpy(e.lexema,"EOF");
+			sprintf(e.lexema,"EOF");
+			t.pe=&e;
 		}
-		else if (c!=EOF)
-		{
-			sprintf(msg,"%c no esperado",c);
-			error(msg);
-		}
-	}
-	if (c==EOF)
-	{
-		t.compLex=EOF;
-		// strcpy(e.lexema,"EOF");
-		sprintf(e.lexema,"EOF");
-		t.pe=&e;
+	
 	}
 	
 }
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-compiladores
+Compiladores
 ============
 
-Compiladores FPUNA
+Compiladores 2017 FP-UNA
 
-Integrantes:
--Marcos Peralta
--Laura Villalba
+####Integrantes: <br>
+-Marcos Peralta marcosperalta.mp@gmail.com<br>
+-Laura Villalba lavi.luari@gmail.com
 
-Para compilar: gcc anlex.c tablaSimbolos.c -o anlex
+####Para compilar:
+1. cd C/
+2. gcc anlex.c tablaSimbolos.c -o anlex
 
