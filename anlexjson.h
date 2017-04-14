5e0c9acbbedaa6f8854c71c46f6185c2691d3195
lauraaw <lavi.luari@gmail.com>
2017-04-14 11:19:33 -0400

    Cabecera

---
+++ anlexjson.h
@@ -0,0 +1,47 @@
+/*********** Librerias utilizadas **************/
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+/************* Definiciones ********************/
+
+//Codigos
+#define STRING		256
+#define NUMBER		257
+#define L_CORCHETE	258
+#define R_CORCHETE	259
+#define L_LLAVE		260
+#define R_LLAVE		261
+#define COMA		262
+#define DOS_PUNTOS	263
+#define PR_TRUE		264
+#define PR_FALSE	265
+#define PR_NULL		266
+// Fin Codigos
+#define TAMBUFF 	5
+#define TAMLEX 		50
+#define TAMHASH 	101
+
+/************* Estructuras ********************/
+
+typedef struct entrada{
+	//definir los campos de 1 entrada de la tabla de simbolos
+	int compLex;
+	char lexema[TAMLEX];	
+	struct entrada *tipoDato; // null puede representar variable no declarada	
+	// aqui irian mas atributos para funciones y procedimientos...
+	
+} entrada;
+
+typedef struct {
+	int compLex;
+	entrada *pe;
+} token;
+
+/************* Prototipos ********************/
+void insertar(entrada e);
+entrada* buscar(const char *clave);
+void initTabla();
+void initTablaSimbolos();
+void sigLex();
